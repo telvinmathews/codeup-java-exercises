ec59ca7c974559e30dbdbea937b05cbc1907fe44
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-08-02 10:50:19 -0500

    finished java syntax exercise

---
+++ src/SytaxTypesVar.java
@@ -0,0 +1,41 @@
+public class SytaxTypesVar {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 7;
+//        System.out.println(myFavoriteNumber);
+        String myString = "My String";
+//        myString = (char) "P"; <--------Can not cast
+//        System.out.println(myString);
+//        myString = 3.14159; <------ Can not change variable types.
+        long myNumber;
+//        System.out.println(myNumber); <------- Can not print a variable that is not assigned any data.
+//        myNumber = 3.14159; <-------- Long type variables can not store decimals.
+//        myNumber = 123L;
+//        myNumber = 123;
+//        float myNumber = 3.14; <-------- Can not change variable type once assigned within the scope.
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x); <-----post increment
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x); <-----pre increment
+
+//        int class <---- can not use reserved words.
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//
+    }
+}
