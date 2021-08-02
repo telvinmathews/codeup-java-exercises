ef00df8584658c36cf95ef36df69a80d2b651f83
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-08-02 16:42:00 -0500

    Finished control statements and loops assignment

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int number = 0;
+//
+//        do {
+//            System.out.println(number);
+//            number+=2;
+//
+//        }
+//        while (number <= 100);
+
+//        int number = 100;
+//
+//        do {
+//            System.out.println(number);
+//            number-=5;
+//
+//        }
+//        while (number >= -10);
+//
+
+//        long number = 2;
+//
+//        do {
+//            System.out.println(number);
+//            number = number * number;
+//
+//        }
+//        while (number <= 1000000);
+
+//        for (long i = 2; i <= 1000000; i*=i) {
+//            System.out.println(i);
+//        }
+
+//        for (long i = 1; i <= 100; i++) {
+////            System.out.println(i);
+//            if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            }
+//            if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            }
+//            if ((i % 3 == 0) && (i % 5 == 0)) {
+//                System.out.println("FizzBuzz");
+//            }
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("What number would you like to go up to? ");
+//        int userNumber = scanner.nextInt();
+//        System.out.println("Here is your table!");
+//        for (int i = 0; i <= userNumber; i++) {
+//           System.out.println(i);
+//        }
+
+        System.out.println("Enter a numerical grade from 0 to 100");
+        Double userGrade = Double.parseDouble(scanner.nextLine());
+        if (userGrade >= 88) {
+            System.out.println("You have an A");
+        } else if (userGrade >= 80 && userGrade <= 87) {
+            System.out.println("You have a B");
+        } else if (userGrade >= 67 && userGrade <= 79) {
+            System.out.println("You have a C");
+        } else if (userGrade >= 60 && userGrade <= 66) {
+            System.out.println("You have a D");
+        } else {
+            System.out.println("You have a F");
+        }
+    }
+}
