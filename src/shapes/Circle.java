1ed15012332080f6ac26d9dc1b48eb164bc23640
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-08-06 10:54:18 -0500

    finished circle circle class

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,25 +1,29 @@
 package shapes;
 
+import java.util.Scanner;
+
 public class Circle {
     //member variables
     private double radius;
+    private Scanner scanner;
 
     //constructor
     public Circle(double radius) {
         this.radius = radius;
+//        this.scanner = new Scanner(System.in);
     }
 
     //member methods
-//
-//    public Circle(double radius) {
-//
-//    }
-//    public double getArea() {
-//
-//    }
-//    public double getCircumference() {
-//
-//    }
+
+
+    public double getArea() {
+        double area = radius * radius * Math.PI;
+        return area;
+    }
+    public double getCircumference() {
+        double circumference= Math.PI * 2*radius;
+        return circumference;
+    }
 
 
 
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,4 +1,7 @@
 package shapes;
 
+import util.Input;
+
 public class CircleApp {
+    Input input = new Input();
 }
